510b23bed7292d0e1babcb69bff5aa27477435b4
Vijeth-V <vijeth1410@gmail.com>
2024-12-10 22:05:35 -0600

    Combined socket server inside the node server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const userRoute = require("./Routes/userRoute")
 const chatRoute = require("./Routes/chatRoute")
 const messageRoute = require("./Routes/messageRoute")
+const { Server } = require("socket.io");
+const e = require("cors")
 
 
 const app = express();
@@ -22,7 +24,7 @@ app.get("/", (req,res) =>{
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLAS_URI;
 
-app.listen(port, (req,res)=>{
+const expressServer = app.listen(port, (req,res)=>{
     console.log(`server is running on port ${port}`)
 });
 
@@ -30,3 +32,41 @@ mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=> console.log("Mongodb connection established !")).catch((error) => console.log("Mongodb connection fail: ", error.message))
+
+const io = new Server(expressServer, { cors: process.env.CLIENT_URL });
+
+let onlineUsers = []
+
+io.on("connection", (socket) => {
+  console.log("View connection",socket.id)
+
+  //listen to connection
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some(user => user.userId === userId) &&
+        onlineUsers.push({
+            userId,
+            socketId: socket.id
+        });
+    console.log("onlineUsers",onlineUsers);
+
+    io.emit("getOnlineUsers", onlineUsers)
+  });
+
+  // add message
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find(user => user.userId === message.recipientId);
+
+    if(user){
+        io.to(user.socketId).emit("getMessage",message);
+        io.to(user.socketId).emit("getNotification",{senderId: message.senderId,
+          isRead: false,
+          date: new Date(),
+        });
+    }
+    });
+
+  socket.on("disconnect", () => {  
+    onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers)
+  });
+});
